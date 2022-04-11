fa5865e722e9b686de6b3e38660f844a7c15d804
Bhawesh Deepak <Bhawesh Deepak@DESKTOP-KB2SSTK>
2022-04-11 00:35:10 -0700

    initial Commit

---
+++ ODI.API/ODI/ODI.API/Helpers/ResponseHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace ODI.API.Helpers
+{
+    public class ResponseHelper<TModel>
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string StatusMessage { get; set; }
+        public List<TModel> TEntities { get; set; }
+        public TModel TEntity { get; set; }
+
+        public ResponseHelper(HttpStatusCode statusCode, string message, List<TModel> entities)
+        {
+            StatusCode = statusCode;
+            StatusMessage = message;
+            TEntities = entities;
+        }
+
+        public ResponseHelper(HttpStatusCode statusCode, string message, TModel entity)
+        {
+            StatusCode = statusCode;
+            StatusMessage = message;
+            TEntity = entity;
+        }
+    }
+
+}
---
+++ ODI.API/ODI/ODI.DataLayer/BaseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ODI.DataLayer
+{
+    public abstract class BaseModel<T>
+    {
+        public T Id { get; set; }
+        public bool IsActive { get; set; } = true;
+        public bool IsDeleted { get; set; } = false;
+        public int CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
