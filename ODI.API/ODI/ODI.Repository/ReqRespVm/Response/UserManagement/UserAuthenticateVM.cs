62fb2313875a3e20307a7f72a364fd556d3136a5
sunilklbs1990 <89584116+sunilklbs1990@users.noreply.github.com>
2022-04-14 12:15:54 +0530

    user management

---
+++ ODI.API/ODI/ODI.Repository/ReqRespVm/Response/UserManagement/UserAuthenticateVM.cs
@@ -0,0 +1,16 @@
+﻿using ODI.DataLayer.UserManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODI.Repository.ReqRespVm.Response.UserManagement
+{
+    public class UserAuthenticateVM
+    {
+        public UserDetails userDetails { get; set; }
+        public bool IsSucess { get; set; }
+        public string Message { get; set; }
+    }
+}
